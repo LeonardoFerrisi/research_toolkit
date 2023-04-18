3b2c4771a90f2a58c20538e9c99ed91ffde22f37
LeonardoFerrisi <biggestresurgence1492@gmail.com>
2023-04-18 07:52:30 -0400

    updated results graphing

---
+++ performance_metrics/condition_vs_accuracy_bargraph_overall/bar_graph.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+def generate_graph(datafilename=None, ascending=False, color='blue', axis_fontsize=12, axis_fontweight='bold'):
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+
+    data_path = os.path.join(script_dir, datafilename)
+
+    # Read in the CSV data
+    df = pd.read_csv(data_path)
+
+    # Group the data by condition and calculate the mean prediction accuracy for each condition
+    mean_acc = df.groupby('Condition')['Prediction Accuracy'].mean()
+    std_acc = df.groupby('Condition')['Prediction Accuracy'].std()
+
+    # Sort the mean accuracy values in descending order
+    mean_acc = mean_acc.sort_values(ascending=ascending)
+    std_acc = std_acc[mean_acc.index]
+
+    # Plot a bar graph of the mean prediction accuracy for each condition
+    ax = mean_acc.plot.bar(yerr=std_acc, color=color, capsize=5)
+    ax.set_xlabel('Condition', fontsize=axis_fontsize, fontweight=axis_fontweight)
+    ax.set_ylabel('Mean Prediction Accuracy', fontsize=axis_fontsize, fontweight=axis_fontweight)
+    ax.set_title('Mean Prediction Accuracy by Condition', fontsize=axis_fontsize, fontweight=axis_fontweight)
+    plt.xticks(rotation=0)
+
+    # Add labels above each bar
+    plt.ylim([0, 1])  # Set y-axis limits
+    labelheight = 0.175
+    for i, v in enumerate(mean_acc):
+        barlabel = str(round((v/1.0)*100, 2))+"%"
+        ax.annotate(barlabel, xy=(i, v + labelheight), ha='center', fontweight='bold')  # Add labels above each bar
+    plt.show()
+
+if __name__ == "__main__":
+    generate_graph(datafilename='conditionvsaccuracy.csv', color='orange')
---
+++ performance_metrics/overtime/overtime_graph.py
@@ -0,0 +1,96 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats import linregress
+import os
+
+def generate_graph(ignore_subject=None, datafilename=None):
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+
+    data_path = os.path.join(script_dir, datafilename)
+
+    # Read in the CSV data
+    df = pd.read_csv(data_path)
+
+    # Set up the plot
+    fig, ax = plt.subplots()
+
+    # Loop over each subject ID and plot the data
+    for subject_id in df['Subject ID'].unique():
+        if not ignore_subject or subject_id not in ignore_subject:
+            sub_df = df[df['Subject ID'] == subject_id]
+            ax.plot(sub_df['Order'], sub_df['Prediction Accuracy'], label=subject_id)
+
+    # Add a trend line for the overall data
+    x = df.groupby(['Order'])['Prediction Accuracy'].mean().index
+    y = df.groupby(['Order'])['Prediction Accuracy'].mean().values
+    z = np.polyfit(x, y, 1)
+    p = np.poly1d(z)
+    ax.plot(x, p(x), '--', color='black', alpha=0.5, linewidth=3,label='Overall')
+
+    # # Set ticks to be only the values contained in order
+    ax.set_xticks([1, 2, 3])
+
+    # Calculate the p-value for the overall trend
+    slope, intercept, r_value, p_value, std_err = linregress(x, y)
+    print("Overall trend p-value:", p_value)
+
+    statistical_test(data_path)
+
+    # tukey_test(data_path)
+
+    # Add a legend and axis labels
+    ax.legend(loc='upper right', title='Subject ID')
+    ax.set_title("Prediction Accuracy vs # of Conditions Tested")
+    ax.set_xlabel('Conditions Tested')
+    ax.set_ylabel('Prediction Accuracy')
+
+    # Show the plot
+    plt.show()
+
+def statistical_test(datapath):
+    import scipy.stats as stats
+
+    print("\n\nPerforming one-way ANOVA test...")
+
+    # Read in the CSV data
+    df = pd.read_csv(datapath)
+
+    # Perform one-way ANOVA test
+    fvalue, pvalue = stats.f_oneway(
+        df[df['Order'] == 1]['Prediction Accuracy'],
+        df[df['Order'] == 2]['Prediction Accuracy'],
+        df[df['Order'] == 3]['Prediction Accuracy']
+    )
+
+    if pvalue < 0.05:
+        print("The ANOVA test is statistically significant (p < 0.05).")
+    else:
+        print("The ANOVA test is not statistically significant (p >= 0.05).")
+
+def tukey_test(datapath):
+    import pandas as pd
+    import statsmodels.api as sm
+    from statsmodels.formula.api import ols
+    from statsmodels.stats.multicomp import MultiComparison
+
+    # Read in the CSV data
+    df = pd.read_csv(datapath)
+
+    # Perform one-way ANOVA
+    model = ols('`Prediction Accuracy` ~ C(Order)', data=df).fit()
+    anova_table = sm.stats.anova_lm(model, typ=2)
+
+    # Calculate the mean prediction accuracy for each order
+    mean_acc = df.groupby('Order').mean()['Prediction Accuracy']
+
+    # Perform all-pairs Tukey test
+    tukey_result = sm.stats.multicomp.pairwise_tukeyhsd(mean_acc.values, mean_acc.index)
+
+    # Print the ANOVA table and Tukey test results
+    print('ANOVA table:\n', anova_table)
+    print('\nTukey test results:\n', tukey_result)
+
+
+if __name__ == '__main__':
+    generate_graph(ignore_subject="T", datafilename='overtime.csv')
